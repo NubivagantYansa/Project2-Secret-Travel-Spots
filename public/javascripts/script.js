4c8ca579a6ca1b2356946405084e57276c14e63a
“Debora <debora.crescenzo@gmail.com>
2020-08-26 10:47:16 +0100

    delete confirmation

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -6,15 +6,18 @@ document.addEventListener(
   false
 );
 
-// document
-//   .getElementById("delete-profile")
-//   .addEventListener("submit", (event) => {
-//     event.preventDefault(); // <= !!! Prevent the refresh
-//     const confirmation = () => {
-//       const result = confirm("Are you sure to delete?");
-//       if (result) {
-//         next();
-//       }
-//     };
-//   });
-
+document.getElementById("delete-profile").addEventListener("click", (event) => {
+  event.preventDefault(); // <= !!! Prevent the refresh
+  // add confirmation screen
+  let str = "";
+  str += `
+    <h2>Are you sure you want to delete your profile?</h2>
+    <p>once you delete your account, you can't recover it.</p>
+    <div>
+    <a href="/user-profile" class="btn btn-primary">No, go back to Profile</a>
+    <form action="/user-profile/{{user._id}}/edit-profile/delete" method="post">
+      <button type="submit" class="btn btn-danger">Yes, delete my Profile</button>
+    </form>
+    </div>`;
+  document.getElementById("main-container").innerHTML = str;
+});
