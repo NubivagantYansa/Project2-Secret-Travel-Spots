2762b81fa5be2a9b7e7e50e5e8f63182798e1129
Raquel Nascimento <raquel.fm.nascimento@gmail.com>
2020-09-09 21:21:40 +0100

    style on most pages

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -8,13 +8,19 @@ document.addEventListener("DOMContentLoaded", () => {
       // add confirmation screen
       let str = "";
       str += `
+      <div class="view text-center p-3 p-md-5 m-md-3">
         <h2>Are you sure you want to delete your profile?</h2>
-        <p>once you delete your account, you can't recover it.</p>
+        <br>
+        <p>Once you delete your account, you can't recover it.</p>
+        <br>
+        <img class="rounded mx-auto d-block image-square" src="../images/sad-cat.gif">
         <div>
-        <a href="/user-profile" class="btn btn-primary">No, go back to Profile</a>
+        <br>
+        <a href="/user-profile" class="btn btn-primary">No, go back to Profile</a> <br><br>
         <form action="/user-profile/{{user._id}}/edit-profile/delete" method="post">
           <button type="submit" class="btn btn-danger">Yes, delete my Profile</button>
         </form>
+        </div>
         </div>`;
       document.getElementById("main-container").innerHTML = str;
     });
